dd3d6aee77eca636b82f46d34fe35577e871f0d4
Michael Schellenberger Costa <miscco@nvidia.com>
2024-05-06 14:19:44 +0200

    Move `uniform_int_distribution` to its own file

---
+++ libcudacxx/test/libcudacxx/std/numerics/rand/rand.dist/rand.dist.uni/rand.dist.uni.int/types.pass.cpp
@@ -0,0 +1,30 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// <random>
+
+// template<class _IntType = int>
+// class uniform_int_distribution
+// {
+//     typedef IntType result_type;
+
+#include <cuda/std/__algorithm_>
+#include <cuda/std/type_traits>
+
+#include "test_macros.h"
+
+int main(int, char**)
+{
+  {
+    typedef cuda::std::uniform_int_distribution<long> D;
+    typedef D::result_type result_type;
+    static_assert((cuda::std::is_same<result_type, long>::value), "");
+  }
+
+  return 0;
+}
