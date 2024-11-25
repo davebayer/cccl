a55cd824d2129757510876673fbbd71e728aa19b
David Bayer <bayer.david99@gmail.com>
2024-11-25 11:31:00 +0100

    change compiler definitions

--- libcudacxx/include/cuda/std/__cccl/compiler.h
+++ libcudacxx/include/cuda/std/__cccl/compiler.h
@@ -57,37 +57,57 @@
      _CCCL_COMPILER_COMPARE_BAD_ARG_COUNT))
 #define _CCCL_COMPILER(...) _CCCL_COMPILER_SELECT(_CCCL_COMPILER_##__VA_ARGS__)(_CCCL_COMPILER_##__VA_ARGS__)
 
+#define _CCCL_CUDA_COMPILER_MAKE_VERSION(_MAJOR, _MINOR) (_MAJOR * 1000 + _MINOR * 10)
+
 // Determine the cuda compiler
 #if defined(__NVCC__)
-#  define _CCCL_CUDA_COMPILER_NVCC
+#  define _CCCL_CUDA_COMPILER_NVCC _CCCL_CUDA_COMPILER_MAKE_VERSION(__CUDACC_VER_MAJOR__, __CUDACC_VER_MINOR__)
+#  define _CCCL_CUDA_ARCH          __CUDA_ARCH__
 #elif defined(_NVHPC_CUDA)
-#  define _CCCL_CUDA_COMPILER_NVHPC
+#  define _CCCL_CUDA_COMPILER_NVHPC _CCCL_CUDA_COMPILER_MAKE_VERSION(__CUDACC_VER_MAJOR__, __CUDACC_VER_MINOR__)
+#  define _CCCL_CUDA_ARCH           __NVCOMPILER_CUDA_ARCH__
 #elif defined(__CUDA__) && _CCCL_COMPILER(CLANG)
-#  define _CCCL_CUDA_COMPILER_CLANG
+#  define _CCCL_CUDA_COMPILER_CLANG _CCCL_CUDACC_MAKE_VERSION(CUDA_VERSION / 1000, (CUDA_VERSION % 1000) / 10)
+#  define _CCCL_CUDA_ARCH           __CUDA_ARCH__
+#elif defined(__CUDACC_RTC__)
+#  define _CCCL_CUDA_COMPILER_NVRTC _CCCL_CUDA_COMPILER_MAKE_VERSION(__CUDACC_VER_MAJOR__, __CUDACC_VER_MINOR__)
+#  define _CCCL_CUDA_ARCH           __CUDA_ARCH__
 #endif
 
-// Shorthand to check whether there is a cuda compiler available
 #if defined(_CCCL_CUDA_COMPILER_NVCC) || defined(_CCCL_CUDA_COMPILER_NVHPC) || defined(_CCCL_CUDA_COMPILER_CLANG) \
-  || _CCCL_COMPILER(NVRTC)
-#  define _CCCL_CUDA_COMPILER
+  || defined(_CCCL_CUDA_COMPILER_NVRTC)
+// Shorthand to check whether there is a cuda compiler available
+#  define _CCCL_HAS_CUDA_COMPILER 1
+
+#  define _CCCL_CUDA_COMPILER_COMPARE_VERSION_1(_COMP) _COMP
+#  define _CCCL_CUDA_COMPILER_COMPARE_VERSION_3(_COMP, _OP, _MAJOR) \
+    _CCCL_CUDA_COMPILER_COMPARE_VERSION_4(_COMP, _OP, _MAJOR, 0)
+#  define _CCCL_CUDA_COMPILER_COMPARE_VERSION_4(_COMP, _OP, _MAJOR, _MINOR) \
+    (_COMP && (_COMP _OP _CCCL_CUDA_COMPILER_MAKE_VERSION(_MAJOR, _MINOR)))
+
+#  define _CCCL_CUDA_COMPILER_SELECT_COUNT(_ARG1, _ARG2, _ARG3, _ARG4, _ARG5, ...) _ARG5
+#  define _CCCL_CUDA_COMPILER_SELECT2(_ARGS)                                       _CCCL_CUDA_COMPILER_SELECT_COUNT _ARGS
+// MSVC traditonal preprocessor requires an extra level of indirection
+#  define _CCCL_CUDA_COMPILER_SELECT(...)        \
+    _CCCL_CUDA_COMPILER_SELECT_COUNT(            \
+      __VA_ARGS__,                               \
+      _CCCL_CUDA_COMPILER_COMPARE_VERSION_4,     \
+      _CCCL_CUDA_COMPILER_COMPARE_VERSION_3,     \
+      _CCCL_CUDA_COMPILER_COMPARE_BAD_ARG_COUNT, \
+      _CCCL_CUDA_COMPILER_COMPARE_VERSION_1,     \
+      _CCCL_CUDA_COMPILER_COMPARE_BAD_ARG_COUNT)
+#  define _CCCL_CUDA_COMPILER(...) \
+    _CCCL_CUDA_COMPILER_SELECT(_CCCL_CUDA_COMPILER_##__VA_ARGS__)(_CCCL_CUDA_COMPILER_##__VA_ARGS__)
+#else
+#  define _CCCL_CUDA_COMPILER(_COMP) 0
 #endif // cuda compiler available
 
-// clang-cuda does not define __CUDACC_VER_MAJOR__ and friends. They are instead retrieved from the CUDA_VERSION macro
-// defined in "cuda.h". clang-cuda automatically pre-includes "__clang_cuda_runtime_wrapper.h" which includes "cuda.h"
-#if defined(_CCCL_CUDA_COMPILER_CLANG)
-#  define _CCCL_CUDACC           1
-#  define _CCCL_CUDACC_VER_MAJOR CUDA_VERSION / 1000
-#  define _CCCL_CUDACC_VER_MINOR (CUDA_VERSION % 1000) / 10
-#  define _CCCL_CUDACC_VER       CUDA_VERSION
-#elif defined(_CCCL_CUDA_COMPILER)
-#  define _CCCL_CUDACC           1
-#  define _CCCL_CUDACC_VER_MAJOR __CUDACC_VER_MAJOR__
-#  define _CCCL_CUDACC_VER_MINOR __CUDACC_VER_MINOR__
-#  define _CCCL_CUDACC_VER       _CCCL_CUDACC_VER_MAJOR * 1000 + _CCCL_CUDACC_VER_MINOR * 10
-#endif // _CCCL_CUDA_COMPILER
-
-#define _CCCL_CUDACC_BELOW(_MAJOR, _MINOR)    (_CCCL_CUDACC && _CCCL_CUDACC_VER < (_MAJOR * 1000 + _MINOR * 10))
-#define _CCCL_CUDACC_AT_LEAST(_MAJOR, _MINOR) (_CCCL_CUDACC && !_CCCL_CUDACC_BELOW(_MAJOR, _MINOR))
+#if defined(_CCCL_CUDA_ARCH)
+#  define _CCCL_CUDA_COMPILATION 1
+#  if defined(__CUDA_ARCH_LIST__)
+#    define _CCCL_CUDA_ARCH_LIST __CUDA_ARCH_LIST__
+#  endif
+#endif
 
 // Convert parameter to string
 #define _CCCL_TO_STRING2(_STR) #_STR
