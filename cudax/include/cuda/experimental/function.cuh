ae7a52aaea8a53651e3e17475d0e65e8ca62fbe1
David Bayer <bayer.david99@gmail.com>
2024-12-08 20:00:21 +0100

    add public headers

---
+++ cudax/include/cuda/experimental/function.cuh
@@ -0,0 +1,17 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of CUDA Experimental in CUDA C++ Core Libraries,
+// under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef __CUDAX_FUNCTION__
+#define __CUDAX_FUNCTION__
+
+#include <cuda/experimental/__function/attributes.cuh>
+#include <cuda/experimental/__function/function_ref.cuh>
+
+#endif // __CUDAX_FUNCTION__
