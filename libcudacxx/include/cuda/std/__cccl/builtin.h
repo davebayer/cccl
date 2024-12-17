7725f05dda54d526607a2ba7d41ca94e4b0652de
David Bayer <bayer.david99@gmail.com>
2024-12-17 08:51:17 +0100

    add builtins

--- libcudacxx/include/cuda/std/__cccl/builtin.h
+++ libcudacxx/include/cuda/std/__cccl/builtin.h
@@ -170,6 +170,24 @@
 #  define _CCCL_BUILTIN_EXPECT(...) __builtin_expect(__VA_ARGS__)
 #endif // _CCCL_CHECK_BUILTIN(builtin_expect)
 
+#if _CCCL_CHECK_BUILTIN(builtin_add_overflow)
+#  define _CCCL_BUILTIN_ADD_OVERFLOW(...) __builtin_add_overflow(__VA_ARGS__)
+#endif // _CCCL_CHECK_BUILTIN(builtin_add_overflow)
+
+#if _CCCL_CHECK_BUILTIN(builtin_sub_overflow)
+#  define _CCCL_BUILTIN_SUB_OVERFLOW(...) __builtin_sub_overflow(__VA_ARGS__)
+#endif // _CCCL_CHECK_BUILTIN(builtin_sub_overflow)
+
+#if _CCCL_CHECK_BUILTIN(builtin_mul_overflow)
+#  define _CCCL_BUILTIN_MUL_OVERFLOW(...) __builtin_mul_overflow(__VA_ARGS__)
+#endif // _CCCL_CHECK_BUILTIN(builtin_mul_overflow)
+
+#if _CCCL_CUDA_COMPILER(NVCC)
+#  undef _CCCL_BUILTIN_ADD_OVERFLOW
+#  undef _CCCL_BUILTIN_SUB_OVERFLOW
+#  undef _CCCL_BUILTIN_MUL_OVERFLOW
+#endif // _CCCL_CUDA_COMPILER(NVCC)
+
 #if _CCCL_CHECK_BUILTIN(builtin_fmax) || _CCCL_COMPILER(GCC)
 #  define _CCCL_BUILTIN_FMAXF(...) __builtin_fmaxf(__VA_ARGS__)
 #  define _CCCL_BUILTIN_FMAX(...)  __builtin_fmax(__VA_ARGS__)
