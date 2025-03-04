84bf08192c2feb26a0c6c3248be394fbd5ee3880
David Bayer <bayer.david99@gmail.com>
2025-03-04 19:15:32 +0100

    fp conversion function

--- libcudacxx/include/cuda/std/__cmath/fp_utils.h
+++ libcudacxx/include/cuda/std/__cmath/fp_utils.h
@@ -27,6 +27,9 @@
 
 _LIBCUDACXX_BEGIN_NAMESPACE_STD
 
+template <class _To, class _From>
+_CCCL_NODISCARD _LIBCUDACXX_HIDE_FROM_ABI _To __cccl_fp_cvt_to(_From __v) noexcept;
+
 // fp32
 
 _CCCL_INLINE_VAR constexpr uint32_t __cccl_fp32_sign_mask     = 0x80000000u;
@@ -46,6 +49,12 @@ __cccl_make_fp32_from_storage(uint32_t __x) noexcept
   return _CUDA_VSTD::bit_cast<float>(__x);
 }
 
+template <>
+_CCCL_NODISCARD _LIBCUDACXX_HIDE_FROM_ABI constexpr float __cccl_fp_cvt_to(float __v) noexcept
+{
+  return __v;
+}
+
 // fp64
 
 _CCCL_INLINE_VAR constexpr uint64_t __cccl_fp64_sign_mask     = 0x8000000000000000ull;
@@ -63,6 +72,12 @@ _CCCL_NODISCARD _LIBCUDACXX_HIDE_FROM_ABI _LIBCUDACXX_CONSTEXPR_BIT_CAST double
 __cccl_make_fp64_from_storage(uint64_t __x) noexcept
 {
   return _CUDA_VSTD::bit_cast<double>(__x);
+}
+
+template <>
+_CCCL_NODISCARD _LIBCUDACXX_HIDE_FROM_ABI constexpr float __cccl_fp_cvt_to(double __v) noexcept
+{
+  return __v;
 }
 
 // nvfp16
@@ -88,6 +103,12 @@ _CCCL_NODISCARD _LIBCUDACXX_HIDE_FROM_ABI constexpr __half __cccl_make_nvfp16_fr
   __cccl_nvfp16_manip_helper __helper{};
   __helper.__x = __x;
   return __helper;
+}
+
+template <>
+_CCCL_NODISCARD _LIBCUDACXX_HIDE_FROM_ABI float __cccl_fp_cvt_to(__half __v) noexcept
+{
+  return ::__half2float(__v);
 }
 #endif // _CCCL_HAS_NVFP16()
 
@@ -115,6 +136,12 @@ _CCCL_NODISCARD _LIBCUDACXX_HIDE_FROM_ABI constexpr __nv_bfloat16 __cccl_make_nv
   __helper.__x = __x;
   return __helper;
 }
+
+template <>
+_CCCL_NODISCARD _LIBCUDACXX_HIDE_FROM_ABI float __cccl_fp_cvt_to(__nv_bfloat16 __v) noexcept
+{
+  return ::__bfloat162float(__v);
+}
 #endif // _CCCL_HAS_NVBF16()
 
 // nvfp8_e4m3
@@ -136,6 +163,19 @@ __cccl_make_nvfp8_e4m3_from_storage(uint8_t __x) noexcept
   __nv_fp8_e4m3 __ret{};
   __ret.__x = static_cast<__nv_fp8_storage_t>(__x);
   return __ret;
+}
+
+template <>
+_CCCL_NODISCARD _LIBCUDACXX_HIDE_FROM_ABI __nv_bfloat16 __cccl_fp_cvt_to(__nv_fp8_e4m3 __v) noexcept
+{
+  return _CUDA_VSTD::make_nvbf16_from_storage(
+    ::__nv_cvt_fp8_to_halfraw(_CUDA_VSTD::__cccl_fp_get_storage(__v), __NV_E4M3).x);
+}
+
+template <>
+_CCCL_NODISCARD _LIBCUDACXX_HIDE_FROM_ABI float __cccl_fp_cvt_to(__nv_fp8_e4m3 __v) noexcept
+{
+  return _CUDA_VSTD::__cccl_fp_cvt_to<float>(_CUDA_VSTD::__cccl_fp_cvt_to<__nv_bfloat16>(__v));
 }
 #endif // _CCCL_HAS_NVFP8_E4M3()
 
@@ -159,6 +199,19 @@ __cccl_make_nvfp8_e5m2_from_storage(uint8_t __x) noexcept
   __ret.__x = static_cast<__nv_fp8_storage_t>(__x);
   return __ret;
 }
+
+template <>
+_CCCL_NODISCARD _LIBCUDACXX_HIDE_FROM_ABI __nv_bfloat16 __cccl_fp_cvt_to(__nv_fp8_e5m2 __v) noexcept
+{
+  return _CUDA_VSTD::make_nvbf16_from_storage(
+    ::__nv_cvt_fp8_to_halfraw(_CUDA_VSTD::__cccl_fp_get_storage(__v), __NV_E5M2).x);
+}
+
+template <>
+_CCCL_NODISCARD _LIBCUDACXX_HIDE_FROM_ABI float __cccl_fp_cvt_to(__nv_fp8_e5m2 __v) noexcept
+{
+  return _CUDA_VSTD::__cccl_fp_cvt_to<float>(_CUDA_VSTD::__cccl_fp_cvt_to<__nv_bfloat16>(__v));
+}
 #endif // _CCCL_HAS_NVFP8_E5M2()
 
 // nvfp8_e8m0
@@ -177,6 +230,18 @@ __cccl_make_nvfp8_e8m0_from_storage(uint8_t __x) noexcept
   __nv_fp8_e8m0 __ret{};
   __ret.__x = static_cast<__nv_fp8_storage_t>(__x);
   return __ret;
+}
+
+template <>
+_CCCL_NODISCARD _LIBCUDACXX_HIDE_FROM_ABI __nv_bfloat16 __cccl_fp_cvt_to(__nv_fp8_e8m0 __v) noexcept
+{
+  return _CUDA_VSTD::make_nvbf16_from_storage(::__nv_cvt_e8m0_to_bf16raw(_CUDA_VSTD::__cccl_fp_get_storage(__v)).x);
+}
+
+template <>
+_CCCL_NODISCARD _LIBCUDACXX_HIDE_FROM_ABI float __cccl_fp_cvt_to(__nv_fp8_e8m0 __v) noexcept
+{
+  return _CUDA_VSTD::__cccl_fp_cvt_to<float>(_CUDA_VSTD::__cccl_fp_cvt_to<__nv_bfloat16>(__v));
 }
 #endif // _CCCL_HAS_NVFP8_E8M0()
 
@@ -201,6 +266,19 @@ __cccl_make_nvfp6_e2m3_from_storage(uint8_t __x) noexcept
   __ret.__x = static_cast<__nv_fp6_storage_t>(__x);
   return __ret;
 }
+
+template <>
+_CCCL_NODISCARD _LIBCUDACXX_HIDE_FROM_ABI __half __cccl_fp_cvt_to(__nv_fp6_e2m3 __v) noexcept
+{
+  return _CUDA_VSTD::make_nvfp16_from_storage(
+    ::__nv_cvt_fp6_to_halfraw(_CUDA_VSTD::__cccl_fp_get_storage(__v), __NV_E2M3).x);
+}
+
+template <>
+_CCCL_NODISCARD _LIBCUDACXX_HIDE_FROM_ABI float __cccl_fp_cvt_to(__nv_fp6_e2m3 __v) noexcept
+{
+  return _CUDA_VSTD::__cccl_fp_cvt_to<float>(_CUDA_VSTD::__cccl_fp_cvt_to<__half>(__v));
+}
 #endif // _CCCL_HAS_NVFP6_E2M3()
 
 // nvfp6_e3m2
@@ -224,6 +302,19 @@ __cccl_make_nvfp6_e3m2_from_storage(uint8_t __x) noexcept
   __ret.__x = static_cast<__nv_fp6_storage_t>(__x);
   return __ret;
 }
+
+template <>
+_CCCL_NODISCARD _LIBCUDACXX_HIDE_FROM_ABI __half __cccl_fp_cvt_to(__nv_fp6_e3m2 __v) noexcept
+{
+  return _CUDA_VSTD::make_nvfp16_from_storage(
+    ::__nv_cvt_fp6_to_halfraw(_CUDA_VSTD::__cccl_fp_get_storage(__v), __NV_E3M2).x);
+}
+
+template <>
+_CCCL_NODISCARD _LIBCUDACXX_HIDE_FROM_ABI float __cccl_fp_cvt_to(__nv_fp6_e3m2 __v) noexcept
+{
+  return _CUDA_VSTD::__cccl_fp_cvt_to<float>(_CUDA_VSTD::__cccl_fp_cvt_to<__half>(__v));
+}
 #endif // _CCCL_HAS_NVFP6_E3M2()
 
 // nvfp4_e2m1
@@ -246,6 +337,19 @@ __cccl_make_nvfp4_e2m1_from_storage(uint8_t __x) noexcept
   __nv_fp4_e2m1 __ret{};
   __ret.__x = static_cast<__nv_fp4_storage_t>(__x);
   return __ret;
+}
+
+template <>
+_CCCL_NODISCARD _LIBCUDACXX_HIDE_FROM_ABI __half __cccl_fp_cvt_to(__nv_fp4_e2m1 __v) noexcept
+{
+  return _CUDA_VSTD::make_nvfp16_from_storage(
+    ::__nv_cvt_fp4_to_halfraw(_CUDA_VSTD::__cccl_fp_get_storage(__v), __NV_E2M1).x);
+}
+
+template <>
+_CCCL_NODISCARD _LIBCUDACXX_HIDE_FROM_ABI float __cccl_fp_cvt_to(__nv_fp4_e2m1 __v) noexcept
+{
+  return _CUDA_VSTD::__cccl_fp_cvt_to<float>(_CUDA_VSTD::__cccl_fp_cvt_to<__half>(__v));
 }
 #endif // _CCCL_HAS_NVFP4_E2M1()
 
