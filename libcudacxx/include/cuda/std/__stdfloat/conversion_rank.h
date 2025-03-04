42a0acc9fd05f08bf3a86010ae44c00ff2da3fda
David Bayer <bayer.david99@gmail.com>
2025-03-04 20:34:57 +0100

    conversion rank

---
+++ libcudacxx/include/cuda/std/__stdfloat/conversion_rank.h
@@ -0,0 +1,82 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of libcu++, the C++ Standard Library for your entire system,
+// under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef _LIBCUDACXX___STDFLOAT_CONVERSION_RANK_H
+#define _LIBCUDACXX___STDFLOAT_CONVERSION_RANK_H
+
+#include <cuda/std/detail/__config>
+
+#if defined(_CCCL_IMPLICIT_SYSTEM_HEADER_GCC)
+#  pragma GCC system_header
+#elif defined(_CCCL_IMPLICIT_SYSTEM_HEADER_CLANG)
+#  pragma clang system_header
+#elif defined(_CCCL_IMPLICIT_SYSTEM_HEADER_MSVC)
+#  pragma system_header
+#endif // no system header
+
+#include <cuda/std/__type_traits/is_same.h>
+#include <cuda/std/limits>
+
+_LIBCUDACXX_BEGIN_NAMESPACE_STD
+
+enum class __fp_conv_rank_order
+{
+  __unordered,
+  __greater,
+  __equal,
+  __less,
+};
+
+template <class _Lhs, class _Rhs>
+_CCCL_NODISCARD _LIBCUDACXX_HIDE_FROM_ABI constexpr __fp_conv_rank_order __fp_make_conv_rank_order()
+{
+  if constexpr (numeric_limits<_Lhs>::is_signed == numeric_limits<_Rhs>::is_signed)
+  {
+    if constexpr (numeric_limits<_Lhs>::min_exponent == numeric_limits<_Rhs>::min_exponent
+                  && numeric_limits<_Lhs>::max_exponent == numeric_limits<_Rhs>::max_exponent
+                  && numeric_limits<_Lhs>::digits == numeric_limits<_Rhs>::digits)
+    {
+#if !defined(_LIBCUDACXX_HAS_NO_LONG_DOUBLE)
+      // If fp64 and long double have the same properties, long double has the higher subrank
+      if constexpr (numeric_limits<double>::min_exponent == numeric_limits<long double>::min_exponent
+                    && numeric_limits<double>::max_exponent == numeric_limits<long double>::max_exponent
+                    && numeric_limits<double>::digits == numeric_limits<long double>::digits)
+      {
+        if constexpr (_CCCL_TRAIT(is_same, _Lhs, long double) && !_CCCL_TRAIT(is_same, _Rhs, long double))
+        {
+          return __fp_conv_rank_order::__greater;
+        }
+        else if constexpr (!_CCCL_TRAIT(is_same, _Lhs, long double) && _CCCL_TRAIT(is_same, _Rhs, long double))
+        {
+          return __fp_conv_rank_order::__less;
+        }
+      }
+#endif // !_LIBCUDACXX_HAS_NO_LONG_DOUBLE
+      return __fp_conv_rank_order::__equal;
+    }
+    else if constexpr (numeric_limits<_Lhs>::min_exponent <= numeric_limits<_Rhs>::min_exponent
+                       && numeric_limits<_Lhs>::max_exponent >= numeric_limits<_Rhs>::max_exponent
+                       && numeric_limits<_Lhs>::digits >= numeric_limits<_Rhs>::digits)
+    {
+      return __fp_conv_rank_order::__greater;
+    }
+    else if constexpr (numeric_limits<_Lhs>::min_exponent >= numeric_limits<_Rhs>::min_exponent
+                       && numeric_limits<_Lhs>::max_exponent <= numeric_limits<_Rhs>::max_exponent
+                       && numeric_limits<_Lhs>::digits <= numeric_limits<_Rhs>::digits)
+    {
+      return __fp_conv_rank_order::__less;
+    }
+  }
+  return __fp_conv_rank_order::__unordered;
+}
+
+_LIBCUDACXX_END_NAMESPACE_STD
+
+#endif // _LIBCUDACXX___STDFLOAT_CONVERSION_RANK_H
